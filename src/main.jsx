d85f274f3ad71c92943f21710402a6fccb873a0e
Gowtham-Mathiyazhagan <gowthammathi07@gmail.com>
2024-06-20 13:24:17 +0530

    Error solved

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "../node_modules/bootstrap-icons/font/bootstrap-icons.min.css";
 import {App} from "./App.jsx";
-import "../src/app.css";
+// import "../src/app.css";
+import './App.css'
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
